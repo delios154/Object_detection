0244c44836eeef234426229bb6fb562d4cba82c4
delios <almansoori.154@gmail.com>
2023-05-26 20:16:36 +0400

    two object identification scripts

--- main.py
+++ main.py
@@ -18,3 +18,37 @@ decoded_predictions = decode_predictions(predictions, top=5)
 # Print the predicted objects and their confidence scores
 for i, (class_id, label, confidence) in enumerate(decoded_predictions[0]):
     print(f"{i+1}. {label}: {confidence * 100:.2f}%")
+
+# from transformers import YolosImageProcessor, YolosForObjectDetection
+# from PIL import Image, ImageDraw
+# import torch
+# import requests
+#
+# # url = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.DcSXIZExW2M45haF40t9pAHaE8%26pid%3DApi&f=1&ipt=b1831aebe55145edcb3cfde593f789f030155ff8f093047c76bbd99f45cd4860&ipo=images"
+# # image = Image.open(requests.get(url, stream=True).raw)
+# image =Image.open('/home/ubuntu/Pictures/123.jpeg')
+#
+# model = YolosForObjectDetection.from_pretrained('hustvl/yolos-tiny')
+# image_processor = YolosImageProcessor.from_pretrained("hustvl/yolos-tiny")
+#
+# inputs = image_processor(images=image, return_tensors="pt")
+# outputs = model(**inputs)
+# _ = model.save_pretrained("./model-dir")
+# # model predicts bounding boxes and corresponding COCO classes
+# logits = outputs.logits
+# bboxes = outputs.pred_boxes
+#
+#
+# # print results
+# target_sizes = torch.tensor([image.size[::-1]])
+# results = image_processor.post_process_object_detection(outputs, threshold=0.9, target_sizes=target_sizes)[0]
+#
+# for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+#     box = [round(i, 2) for i in box.tolist()]
+#     draw = ImageDraw.Draw(image)
+#     draw.rectangle(box)
+#     print(
+#         f"Detected {model.config.id2label[label.item()]} with confidence "
+#         f"{round(score.item(), 3)} at location {box}"
+#     )
+# image.show()
